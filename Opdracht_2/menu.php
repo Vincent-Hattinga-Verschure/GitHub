e490e05af40a5cef90c9d918bf2e4ad334d9ea0d
DESKTOP-TD3CBI9\vince <vincenthattinga@gmail.com>
2017-05-11 19:21:59 +0200

    opdracht 2

---
+++ Opdracht_2/menu.php
@@ -0,0 +1,7 @@
+<div CLASS="a11" >
+	<ul>
+		<li><a href="form.php">Er heerst paniek.....</a></li>
+		<li><a href="onkunde.php">Onkunde</a></li>
+		
+	</ul>
+</div>
---
+++ Opdracht_2/menu2.php
@@ -0,0 +1,7 @@
+<div CLASS="a11" >
+	<ul>
+		<li><a href="form.php">Terug</a></li>
+		
+		
+	</ul>
+</div>
