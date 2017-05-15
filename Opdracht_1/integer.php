93ccad760aa8b6b02f3283723570e2ae8ee55fe2
DESKTOP-TD3CBI9\vince <vincenthattinga@gmail.com>
2017-05-15 10:09:32 +0200

    Opdracht_1

--- Opdracht_1/integer.php
+++ Opdracht_1/integer.php
@@ -5,11 +5,12 @@
 <link href="style.css" rel="stylesheet" type="text/css" >
 
 <body>
+
 <div CLASS="c1">
 
 
 	
-	<?php
+<?php
   include 'menu.php';
 ?>
 
@@ -21,9 +22,9 @@
 
 <DIV  CLASS="c2" >
 <img src="Images/integer1.jpg">
-
-	<title>Integer volutpat</title>
-	<h1>Integer volutpat</h1>
+<title>Integer volutpat</title>
+<h1>Integer volutpat</h1>		
+	<p>
 	<img src="Images/integer2.jpg" align="left">
 	<p>Nam mollis tortor vel velit dignissim eleifend. Vivamus mattis interdum eros, a semper neque vehicula non. Morbi risus eros, ornare nec mollis ut, egestas a lorem. Duis tincidunt imperdiet massa, eu ullamcorper augue elementum et. <img src="Images/integer3.jpg" align="right">Donec rhoncus enim quis nunc malesuada vitae sollicitudin enim pharetra. Sed at eros ligula. Suspendisse malesuada volutpat gravida. Donec hendrerit ipsum risus. Curabitur luctus lectus et velit rutrum tincidunt ac non urna. Praesent iaculis lacus id turpis aliquam ornare. Cras tempor arcu nisi. Quisque sollicitudin laoreet fermentum. Fusce gravida, sapien porta condimentum lacinia, enim risus elementum ante, vitae consectetur felis urna ac est. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
 
